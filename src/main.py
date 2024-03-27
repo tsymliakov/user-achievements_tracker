d148776886944324989d2946ed609d4075e86edb
tsymliakov <denis.tsym@mail.ru>
2024-03-27 17:14:01 +0300

    feat: deleted unused code from main.py

--- src/main.py
+++ src/main.py
@@ -1,73 +1,14 @@
 from fastapi import FastAPI
-from src.database import Session
-
-from src.models import User, Achievment, User_Achievment
+from src.presentation.rest.routers import users_router, achievment_router
 
 
 app = FastAPI()
 
 
+app.include_router(users_router)
+app.include_router(achievment_router)
+
+
 @app.get("/")
 def main():
     return {"data": "Hello!"}
-
-# @app.get("/users/")
-# def get_users():
-#     with Session() as session:
-#         users = session.query(TestUser).all()
-#     return users
-
-# @app.get("/users/add")
-# def add_user():
-#     with Session() as session:
-#         new_user = TestUser(name="John")
-#         session.add(new_user)
-#         session.commit()
-
-
-# Информация о пользователе
-@app.get("/users/{id}")
-def get_user(id: int):
-    with Session() as session:
-        user = session.query(User).all()
-    return user
-
-
-# # Все доступные достижения
-# @app.get("/achievments")
-# def get_all_achievments():
-#     return "Achievments"
-
-# # Добавить новое достижение
-# @app.post("/achievments/add")
-# def add_achievment():
-#     pass
-
-# # Добавить пользователю достижение
-# @app.post("/users/{user_id}/{achievment_id}")
-# def add_achievment_to_user():
-#     pass
-
-# # предоставлять информацию о выданных пользователю достижениях на выбранном пользователем языке
-
-# # Пользователь с максимальным количеством достижений
-# @app.get("/users/max_achievments")
-# def get_user_max_achievments():
-#     pass
-
-# # Пользователь с максимальным количеством очков достижений;
-# @app.get("/users/max_score")
-# def get_user_max_score():
-#     pass
-
-# # Пользователи с максимальной разностью очков достижений;
-# @app.get("/users/max_difference_score")
-# def get_users_max_diff():
-#     pass
-
-# # Пользователи с минимальной разностью очков достижений;
-# @app.get("/users/min_difference_score")
-# def get_users_min_diff():
-#     pass
-
-# # Пользователи, у которых достижения выдавались 7 дней подряд (есть достижения с датами выдачи соответсвующими)
